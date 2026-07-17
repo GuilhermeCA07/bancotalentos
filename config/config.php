3ea7ce0a0a99b10b4c815f0f3d11ead6eb330f74
Guilherme Coelho Araujo <108228925+GuilhermeCA07@users.noreply.github.com>
2026-07-17 14:43:42 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 define(
     'TURNSTILE_SITE_KEY',
-    '0x4AAAAAADp1NonOnccCYXZy'
+    ''
 );
 
 define(
     'TURNSTILE_SECRET',
-    '0x4AAAAAADp1NpaATnwPLyDQsUYu6yAtJmU'
+    ''
 );
 
 define(
